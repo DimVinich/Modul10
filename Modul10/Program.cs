63acf69cb8b02263637a492d02e2d75b39ea7fe1
Dmitrij Sokolov <DmitrijVS@gmail.com>
2022-07-05 16:10:59 +0300

    Добавьте файлы проекта.

---
+++ Modul10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modul10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
